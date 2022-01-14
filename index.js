b23915eb88420530c3526efdfa361c2c5bddb027
quangngonz <46818455+quangngonz@users.noreply.github.com>
2022-01-14 12:19:55 +0700

    Add catch error

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ console.log("Hosting on port " + process.env.PORT || 3000);
 console.log("URL: http://localhost:".concat(process.env.PORT || 3000));
 
 const KtoC = (K) => {
-  return K - 273.15;
+  return Math.floor(K - 273.15);
 };
 
 app.get("/", function (req, res) {
@@ -20,16 +20,24 @@ app.get("/weather", function (req, res) {
   const city = req.query.city;
   const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
   console.log(url);
-  axios.get(url).then(function (response) {
-    res.send(
-      "The weather temperature in " +
-        city +
-        " is " +
-        KtoC(response.data.main.temp) +
-        " the weather description is " +
-        response.data.weather[0].description
-    );
-  });
+  axios
+    .get(url)
+    .then(function (response) {
+      res.send(
+        "The weather temperature in " +
+          city +
+          " is " +
+          KtoC(response.data.main.temp) +
+          " the weather description is " +
+          response.data.weather[0].description
+      );
+    })
+    .then(() => {
+      console.log("Request completed");
+    })
+    .catch(function (error) {
+      res.send("Error: " + error);
+    });
 });
 
 app.listen(process.env.PORT || 3000);
